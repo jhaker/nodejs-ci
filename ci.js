071aa58338cc565470e95b7167db12e7ee90ae63
jhaker@yahoo.com <jhaker@yahoo.com>
2014-02-17 21:14:32 -0600

    initial ci checkin

--- ci.js
+++ ci.js
@@ -1,6 +1,35 @@
-
+var queue = require('./queue'),
+	msbuild = require('msbuild'),
+	gitwin = require('gitwin');
+	
 var ci = function(){
-	//put code here
+	this.queue = queue;
+	this.gitwin = gitwin;
+	this.msbuild = msbuild;
+	this.bitci = function(config){
+
+	this.publish = function(callback){ 
+		var _msbuild = new msbuild(callback);
+		_msbuild.setConfig(config);		
+		_msbuild.verbose = false;
+		_msbuild.publish();
+	 };
+	this.build = function(callback){
+		var _msbuild = new msbuild(callback);
+		_msbuild.setConfig(config);		
+		_msbuild.verbose = false;
+		_msbuild.build();
+	 };
+	this.pull =  function(callback){
+		var _gitwin = new gitwin(callback);
+		_gitwin.path = (config.watchPath);
+		_gitwin.verbose = true;
+		_gitwin.pull();
+	};
+};
 }
 
-module.exports = ci;
+module.exports = new ci();
+
+
+
